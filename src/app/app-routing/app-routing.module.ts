3a19d9cb24ba7458fd06abc7c1c2342203176729
Vallabh Kansagara <vrkansagara@gmail.com>
2018-08-07 01:31:47 +0530

    Adding layout components

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -6,21 +6,36 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from '../home/home.component';
 import { PageNotFoundComponent } from '../page-not-found/page-not-found.component'
+import { SiteLayoutComponent } from '../_layout/site-layout/site-layout.component'
+import { AboutComponent } from '../page/about/about.component';
 
-const routes: Routes = [
-  { path: '',   redirectTo: 'home', pathMatch: 'full' },
-  {
-    path: 'home',
-    component: HomeComponent,
-    pathMatch: 'full'
-  },
-  { path: '**', component: PageNotFoundComponent }
+
+
+const appRoutes: Routes = [
+    
+    //Site routes goes here 
+    { 
+        path: '', 
+        component: SiteLayoutComponent,
+        children: [
+          { path: '', component: HomeComponent, pathMatch: 'full'},
+          { path: 'home', component: HomeComponent },
+          { path: 'about', component: AboutComponent }
+        ]
+    },
+    
+
+    //no layout routes
+    { path: '**', component: PageNotFoundComponent }
+    // { path: '**', redirectTo: '' } // otherwise redirect to home
 ];
+
+const routing = RouterModule.forRoot(appRoutes);
 
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    routing
   ],
   exports: [
     RouterModule
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,23 @@ import { AppComponent } from './app-component/app.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './_layout/header/header.component';
+import { FooterComponent } from './_layout/footer/footer.component';
+import { LeftSidebarComponent } from './_layout/left-sidebar/left-sidebar.component';
+import { RightSidebarComponent } from './_layout/right-sidebar/right-sidebar.component';
+import { SiteLayoutComponent } from './_layout/site-layout/site-layout.component';
+import { AboutComponent } from './page/about/about.component';
 @NgModule({
   declarations: [
     AppComponent,
     PageNotFoundComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    LeftSidebarComponent,
+    RightSidebarComponent,
+    SiteLayoutComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
